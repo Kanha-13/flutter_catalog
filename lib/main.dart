ca9be8c86c94ef1697557ee08f4b59a4a26f148e
Kanha Agrawal <kanha.agr11@gmail.com>
2021-06-04 18:49:36 +0530

    cahnged the tutorial from where i was learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,23 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
-int main() {
-  runApp(MyApp());
-  return 0;
+void main() {
+  runApp(MaterialApp(
+    title: "First_App",
+    home: HomePage(),
+  ));
 }
 
-//we creaated this methot to run out appp "MyApp" is the name of our app
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  //ui related settings
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to my app"),
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My first flutter app"),
+      ),
+      body: Container(
+        child: Center(child: Text("Hello flutter")),
       ),
     );
   }
