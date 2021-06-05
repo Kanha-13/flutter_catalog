c7f1aec45bffde4063448ff755e672aa684330ac
Kanha Agrawal <kanha.agr11@gmail.com>
2021-06-05 21:40:22 +0530

    donw with drawer and basic user account image in drawer head

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "First_App",
     home: HomePage(),
+    theme: ThemeData(primarySwatch: Colors.purple),
   ));
 }
 
@@ -16,113 +19,39 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("My first flutter app"),
       ),
-      body: Container(
-        alignment: Alignment.topCenter,
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Container(
-            color: Colors.black,
-            width: 300,
-            height: 300,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  // padding: const EdgeInsets.all(28),
-                  alignment: Alignment.center,
-                  // color: Colors.red,
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    //to change box shape use shape
-                    // shape: BoxShape.circle,
-                    //to change box shape with side radius use border radius
-                    borderRadius: BorderRadius.circular(8),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey.shade400,
-                          blurRadius: 8,
-                          offset: Offset(3.0, 8.0))
-                    ],
-                    color: Colors.red,
-                    gradient:
-                        LinearGradient(colors: [Colors.yellow, Colors.pink]),
-                  ),
-                  child: Text(
-                    "I am first box",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                    ),
-                  ),
-                ),
-                Container(
-                  // padding: const EdgeInsets.all(28),
-                  alignment: Alignment.center,
-                  // color: Colors.red,
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    //to change box shape use shape
-                    // shape: BoxShape.circle,
-                    //to change box shape with side radius use border radius
-                    borderRadius: BorderRadius.circular(8),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey.shade400,
-                          blurRadius: 8,
-                          offset: Offset(3.0, 8.0))
-                    ],
-                    color: Colors.red,
-                    gradient:
-                        LinearGradient(colors: [Colors.yellow, Colors.pink]),
-                  ),
-                  child: Text(
-                    "I am second box",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                    ),
-                  ),
-                ),
-                Container(
-                  // padding: const EdgeInsets.all(28),
-                  alignment: Alignment.center,
-                  // color: Colors.red,
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    //to change box shape use shape
-                    // shape: BoxShape.circle,
-                    //to change box shape with side radius use border radius
-                    borderRadius: BorderRadius.circular(8),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey.shade400,
-                          blurRadius: 8,
-                          offset: Offset(3.0, 8.0))
-                    ],
-                    color: Colors.red,
-                    gradient:
-                        LinearGradient(colors: [Colors.yellow, Colors.pink]),
-                  ),
-                  child: Text(
-                    "I am third box",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                    ),
-                  ),
-                ),
-              ],
-            )),
+      body: Container(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Kanha Agrawal"),
+              accountEmail: Text("Kanha.agr11@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1622832148332-b436648cef62?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80"),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("kanha.agr11@gmail.com"),
+              trailing: Icon(Icons.send),
+            ),
+          ],
+        ),
+      ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+        backgroundColor: Colors.purple,
       ),
     );
   }
